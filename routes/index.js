332bc4e75eca4d32078034278e883b9b09749719
Elias Bhuiyan <91391344+eliasbhuiyan@users.noreply.github.com>
2025-06-03 16:32:00 +0600

    route

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,10 @@ const router = express.Router();
 
 router.use("/api/v1" , apiRoute)
 
+router.get("/", (req, res)=>{
+    res.send("Hello! Welcome to the server.")
+})
+
 router.use((req, res)=>{
     res.status(404).send("Page not found!")
 })
